0980781d42854b9e79c112bc311bc1ccf6be9563
Induja Karalliyadda <indujakaralliyadda99@gmail.com>
2024-04-04 09:39:05 +0530

    create a add new book

--- src/app/page/view-allbook/add-books/add-books.component.ts
+++ src/app/page/view-allbook/add-books/add-books.component.ts
@@ -1,16 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { NavComponent } from '../../../common/nav/nav.component';
 
 @Component({
   selector: 'app-add-books',
   standalone: true,
-  imports: [HttpClientModule, FormsModule, CommonModule],
+  imports: [HttpClientModule, FormsModule, CommonModule,NavComponent],
   templateUrl: './add-books.component.html',
   styleUrl: './add-books.component.css'
 })
+ 
 export class AddBooksComponent {
+ constructor(private http:HttpClient){
+
+ }
   public book: any = {
     isbn:"",
     title:"",
@@ -20,5 +26,15 @@ export class AddBooksComponent {
   }
   addBook() {
     console.log(this.book)
+    this.http.post("http://localhost:8080/book/add",this.book).subscribe(data=>{
+      console.log("add Book");
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: `${this.book.title} Book Added`,
+        showConfirmButton: false,
+        timer: 1500
+      });
+    })
   }
 }
